fe999730008e497f0994364281654d7a3e4c595b
ayomideAlaka <muizzyomide@gmail.com>
2024-04-10 19:44:32 +0100

    Made changes to .rb

--- Formula/retrodeep-cli.rb
+++ Formula/retrodeep-cli.rb
@@ -4,15 +4,15 @@ class RetrodeepCli < Formula
   desc "RetroDeep CLI is a powerful, user-friendly command-line interface designed to supercharge your development workflow by enabling you to deploy, manage, and scale your applications directly from the terminal." 
   homepage "https://github.com/retrodeep-io/retrodeep-cli"
   url "https://github.com/retrodeep-io/retrodeep-cli/archive/refs/tags/v1.0.0-beta.1.tar.gz"
-  sha256 "2763632a5d5902ed05ebd1e02dace54842553fccfdc02132bcc5660b9896abdd"
+  sha256 "7c2a3bc9265f8e7c2ab72b7480e540cc208d471dc53e1dc2f8f057600c8b99c2"
   license "MIT"
-  version "1.0.0-beta.1"
+  version "1.0.0-beta.2"
 
   depends_on "python@3"
 
   def install
       virtualenv_create(libexec, "python3")
-      system libexec/"bin/pip", "install", "-r", "requirements.txt", "--user"
+      system libexec/"bin/pip", "install", "-r", "requirements.txt"
 
       bin.install_symlink libexec/"bin/retrodeep" => "retrodeep"
   end
