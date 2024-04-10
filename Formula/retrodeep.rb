22fb2fce73115192c1c758d3958433dd81be64c6
ayomideAlaka <muizzyomide@gmail.com>
2024-04-10 17:11:39 +0100

    Added url to tar

--- Formula/retrodeep.rb
+++ Formula/retrodeep.rb
@@ -3,7 +3,7 @@ class Retrodeep < Formula
 
   desc "RetroDeep CLI is a powerful, user-friendly command-line interface designed to supercharge your development workflow by enabling you to deploy, manage, and scale your applications directly from the terminal." 
   homepage "https://github.com/retrodeep-io/retrodeep-cli"
-  url ""
+  url "https://github.com/retrodeep-io/retrodeep-cli/archive/refs/tags/v1.0.0-beta.1.tar.gz"
   sha256 "2763632a5d5902ed05ebd1e02dace54842553fccfdc02132bcc5660b9896abdd"
   license "MIT"
   version "1.0.0-beta.1"
